0fe15018028674c20f5ec63277e7ce165b2d5509
Kae <kaehehehe@gmail.com>
2022-02-09 22:42:59 +0900

    :art: prettier 적용

--- src/print.js
+++ src/print.js
@@ -14,10 +14,10 @@ export function showRouteMap() {
     const h3 = document.createElement('h3');
     h3.textContent = obj.name;
     map.append(h3);
-    obj.list.forEach(item => {
+    obj.list.forEach((item) => {
       const li = `<li>${item}</li>`;
       ul.insertAdjacentHTML('beforeend', li);
-    })
-    map.append(ul)
+    });
+    map.append(ul);
   }
 }
